f6eb1afe8cb22216fc25002a0498d02e7bc54c36
schmid55 <schmid55@wwu.edu>
2021-01-12 13:56:23 -0800

    Updating to use config file over hardcoded channel ids

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 require('dotenv').config();
 const Discord = require('discord.js');
-const { prefix, foyer, office } = require('./config.json');
+const { prefix, foyer, office, bottext } = require('./config.json');
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
@@ -14,13 +14,14 @@ client.once('ready', () => {
 
 let waiting = [];
 client.on('message', message => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    if (message.channel.id !== bottext || !message.content.startsWith(prefix) || message.author.bot) return;
     
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
     const commandName = args.shift().toLowerCase();
     
     switch (commandName) {
         case 'next':
+            // TODO: Check if role matches
             if (!waiting.length) {
                 message.channel.send("Queue is empty");
             } else {
@@ -48,8 +49,8 @@ client.on('message', message => {
 client.on('voiceStateUpdate', (oldMember, newMember) => {
     if (oldMember.channel !== newMember.channel) {
         if (newMember.channel) {
-            if (newMember.channel.name == "The Foyer") {
-                client.channels.fetch("798399020136792086").then(channel => {
+            if (newMember.channel.id === foyer) {
+                client.channels.fetch(bottext).then(channel => {
                     channel.send(`${newMember.member.displayName} has joined ${newMember.channel.name}`)
                 });
                 waiting.push(newMember.member);
@@ -57,7 +58,7 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
             }
         }
         if (oldMember.channel) {     
-            if (oldMember.channel.name === "The Foyer" && !newMember.channel || newMember.channel.name !== "The Foyer") {
+            if (oldMember.channel.id === foyer && !newMember.channel || newMember.channel.id !== foyer) {
                 const index = waiting.indexOf(newMember.member);
                 if (index !== -1) {
                     waiting.splice(index, 1);
